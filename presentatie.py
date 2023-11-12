de88a0a481a6bc5642368ebb46eb9ffb1732f283
Hans van der Linde <hansvanderlinde@yahoo.nl>
2023-11-12 19:29:24 +0100

    versie_tm_les10

---
+++ presentatie.py
@@ -0,0 +1,8 @@
+def presenteer (totaal):
+    for k,v in dict.items(dict):
+        print (k,v)
+    print (26*"=")
+    d = sum(dict.values)
+    print ("Totaal:", d, "euro")
+
+    return presenteer(totaal)
